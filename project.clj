73e5051ad790996eabc8c62185e086353883379f
Mike Sperber <sperber@deinprogramm.de>
2014-02-13 09:23:56 +0100

    Clean up project.clj.

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject cljs-hello "0.1.0-SNAPSHOT"
-  :description "FIXME: write this!"
-  :url "http://example.com/FIXME"
+  :description "Introductory ClojureScript example for funktionale-programmierung.de"
 
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-2138"]]
